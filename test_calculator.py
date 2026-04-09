51d4975a89d459c860e669801db7261b367d4ae0
grayson-burns11 <grayson.burns@ufl.edu>
2026-04-09 15:10:45 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -11,12 +11,16 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(2, 5), 10)
+        self.assertEqual(mul(0, 7), 0)
+        self.assertEqual(mul(-6, 3), -18)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(10, 5), 2)
+        self.assertEqual(div(20, 4), 5)
+        self.assertEqual(div(16, 2), 8)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,22 +38,22 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+                logarithm(0, 24)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(5, 12), 13)
+        self.assertEqual(hypotenuse(6, 8), 10)
+
+    def test_sqrt(self): # 3 assertions
+
+        with self.assertRaises(ValueError):
+            square_root(-2)
+        self.assertEqual(square_root(4), 2)
+        self.assertEqual(square_root(16), 4)
 
 # Do not touch this
 if __name__ == "__main__":
