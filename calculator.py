15445352c663c90472632e3a444c465831fad245
chuks121 <chukwuekpebegh@gmail.com>
2026-04-13 19:39:04 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -22,3 +22,32 @@ def logarithm(a, b):
     return math.log(a,b)
 def exponent(a, b):
     return a**b
+
+import math
+
+def square_root(a):
+    try:
+        return math.sqrt(a)
+    except ValueError:
+        raise ValueError("Can't take square root of negative numbers")
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)
+    except TypeError:
+        raise TypeError("Inputs must be numbers")
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by zero")
+    return b / a
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Incorrect input for logarithm")
+    return math.log(b, a)
+def exp(a, b):
+    return a ** b
