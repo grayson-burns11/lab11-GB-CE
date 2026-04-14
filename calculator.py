5ca187ecea00c650e645bea3132e84dc6b1bee0e
grayson-burns11 <grayson.burns@ufl.edu>
2026-04-13 20:24:01 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -15,7 +15,7 @@ def hypotenuse(a, b):
         raise TypeError("Inputs must be numbers")
 def add(a, b):
     return a + b
-def sub(a, b):
+def subtract(a, b):
     return a - b
 def mul(a, b):
     return a * b
@@ -23,7 +23,7 @@ def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Can't divide by zero")
     return b / a
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Incorrect input for logarithm")
     return math.log(b, a)
